fabcb6c9c204234d3d709187b2a221a8c33d6316
Alexander Graul <alexander.graul@posteo.de>
2016-07-08 01:27:42 +0200

    some login changes

--- simple_bot.py
+++ simple_bot.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 
 import discord
 client = discord.Client()
 
-def region_assign(author, message):
+# def assign_region (author, message):
 
 
 @client.event
 def on_message(message):
-    authoer = message.author
-    if message.content.startswith('!region'):
-        region_assign()
+    author = message.author
+    # if message.content.startswith('!region'):
+    # assign_region ()
+    if message.content.startswith('!tetst'):
+        client.send_message(message.channel,
+                            "%s, How are you doing ?" % author)
 
-client.accept_invites('')
-client.run()
+client.run('MTk5NDY2MzA2ODc2MDgwMTI4.CmBsyg.pcJmTTObQn-cbDZcgGWRdXTjUq8')

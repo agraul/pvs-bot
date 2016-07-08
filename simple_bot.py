42cfbc5e80a76d7a257905696f775def005436b8
Alexander Graul <alexander.graul@posteo.de>
2016-07-08 02:31:20 +0200

    bot can go online

--- simple_bot.py
+++ simple_bot.py
@@ -5,16 +5,16 @@
 import discord
 client = discord.Client()
 
-# def assign_region (author, message):
+
+def assign_region(author, message):
+    region = message.content.strip('!region')
+    client.send_message(message.channel, "You chose: %s " % region)
 
 
-@client.event
+@client.async_event
 def on_message(message):
     author = message.author
-    # if message.content.startswith('!region'):
-    # assign_region ()
-    if message.content.startswith('!tetst'):
-        client.send_message(message.channel,
-                            "%s, How are you doing ?" % author)
+    if message.content.startswith('!region'):
+        assign_region(author, message)
 
 client.run('MTk5NDY2MzA2ODc2MDgwMTI4.CmBsyg.pcJmTTObQn-cbDZcgGWRdXTjUq8')

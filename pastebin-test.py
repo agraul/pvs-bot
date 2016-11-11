4b1e0c1947e820fa050c082ac2e932e1a41b4d40
Alexander Graul <alexander.graul@posteo.de>
2016-11-11 15:52:10 +1100

    changed author, debugging output

--- pastebin-test.py
+++ pastebin-test.py
@@ -3,8 +3,13 @@ import subprocess
 import pyperclip
 import shlex
 
-command_line = 'pastebinit -b pastebin.com -i testfile.txt'
+''' What happens:
+    printed: <function _pasteXclip at 0x..> inside python
+    new shell output/python session: link
+    stuck inside python
+'''
+command_line = 'pastebinit -a PvS-Bot -b pastebin.com -i testfile.txt'
 args = shlex.split(command_line)
 p = subprocess.Popen(args)
 link = pyperclip.paste
-print(link)
+#print(link)

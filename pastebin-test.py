1337979cfc2ab8f1c3fc9ea6d57f249158691486
Alexander Graul <alexander.graul@posteo.de>
2016-11-11 16:00:25 +1100

    python3.5: subprocess.run() test

--- pastebin-test.py
+++ pastebin-test.py
@@ -3,13 +3,11 @@ import subprocess
 import pyperclip
 import shlex
 
-''' What happens:
-    printed: <function _pasteXclip at 0x..> inside python
-    new shell output/python session: link
-    stuck inside python
+
 '''
 command_line = 'pastebinit -a PvS-Bot -b pastebin.com -i testfile.txt'
 args = shlex.split(command_line)
 p = subprocess.Popen(args)
 link = pyperclip.paste
-#print(link)
+'''
+subprocess.run(["pastebinit", "-a", "PvS-Bot", "-b", "pastebin.com", "-i", "testfile.txt"], stdout=subprocess.PIPE)

8b16755a9ba90f7f7df495de32e5e99a8bce154b
Alexander Graul <alexander.graul@posteo.de>
2017-07-27 23:33:25 +0200

    add channel info to logging

--- AdminTools.py
+++ AdminTools.py
@@ -15,22 +15,22 @@ import RoleManagement
 async def log_message(client, message, chatlog, utc, forbidden):
     if message.channel not in forbidden:
         await client.send_message(
-            chatlog, "{}UTC: {} `MESSAGE` - {}".format(
-                utc, message.author, message.content))
+            chatlog, "**{}UTC: {}:{}** `MESSAGE` - {}".format(
+                utc, message.channel, message.author, message.content))
 
 
 async def log_message_edit(client, old, new, chatlog, utc, forbidden):
     if message.channel not in forbidden:
         await client.send_message(
-            chatlog, "{}UTC: {} `EDIT` - {} `TO` {}".format(
-               utc, old.author, old.content, new.content))
+            chatlog, "**{}UTC: {}:{}** `EDIT` - {} `TO` {}".format(
+               utc, old.channel, old.author, old.content, new.content))
 
 
 async def log_message_delete(client, message, chatlog, utc, forbidden):
     if message.channel not in forbidden:
         await client.send_message(
-            chatlog, "{}UTC: {} `DELETED` - {}".format(
-                utc, message.author, message.content))
+            chatlog, "**{}UTC: {}:{}** `DELETED` - {}".format(
+                utc, message.channel, message.author, message.content))
 
 # TODO: welcome_private
 
